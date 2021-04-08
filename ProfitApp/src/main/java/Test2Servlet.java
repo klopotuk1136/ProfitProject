d91cd135546730c9905102a9fde462042f767fda
Pavel <klopotuk@gmail.com>
2021-04-09 00:17:07 +0300

    Created maven webApp ProfitApp with servlets from task5, changed gitignore for this directory

---
+++ ProfitApp/src/main/java/Test2Servlet.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+
+public class Test2Servlet extends HttpServlet {
+    private static final String PATH = "/page";
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        response.sendRedirect(PATH);
+    }
+}

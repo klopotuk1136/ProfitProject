717b996452976ff013d8c6bd41cd88ed877223d1
klopotuk <klopotuk@gmail.com>
2021-05-08 20:59:11 +0300

    Created special folders for files, added global functions for work with page. Cleaned code from unnecessary lines

--- UI/src/JS/script.js
+++ UI/src/JS/script.js
@@ -1,184 +1,3 @@
-class AdsList{
-    _adList = [];
-    constructor(adList){
-        this._adList = adList.concat();
-    }
-
-    getPage(skip = 0, top = 10, filterConfig = undefined){
-        if (typeof skip !== 'number' || typeof top !== 'number') {
-            console.log('Error with inputting types!');
-            return;
-        }
-    
-        let returningAds = this._adList;
-        if (filterConfig){
-        
-            for (let parameter in filterConfig){
-                if (parameter === 'hashTags'){
-                    filterConfig.hashTags.forEach(tag => {
-                        returningAds = returningAds.filter(ad => ad.hashTags.includes(tag));
-                    });
-                }
-                if (parameter === 'dateFrom'){
-                    returningAds = returningAds.filter(ad => ad.createdAt >= filterConfig.dateFrom);
-                }
-                else if (parameter === 'dateTo'){
-                    returningAds = returningAds.filter(ad => ad.createdAt < filterConfig.dateTo);
-                }
-                else if (parameter === 'vendor'){
-                    returningAds = returningAds.filter(ad => ad.vendor === filterConfig.vendor);
-                }
-            }
-        }
-            
-        returningAds.sort(this._comparator);
-        return returningAds.slice(skip, skip + top);
-    
-    }
-
-    _comparator(first, second){
-        return second.createdAt - first.createdAt;
-    }
-
-    get(id){
-        if (typeof id === 'string'){
-            return this._adList.find(ad => ad.id === id)
-        }
-        console.log('Incorrect type of id. Id must be a string.');
-    }
-
-    static validate(ad){
-        let requiredParams = ['id','label', 'description', 'createdAt', 'link', 'vendor', 'hashTags', 'discount', 'validUntil'];
-        for (let i = 0; i < requiredParams.length; i++) {
-            if (ad[requiredParams[i]] === undefined) {
-                return false;
-            }
-        }
-        for (let param in ad){
-            switch (param) {
-                case 'id':
-                    if (typeof ad.id !== 'string' || ad.id.length === 0) {
-                        return false;
-                    }
-                    break;
-                case 'label':
-                    if (typeof ad.label !== 'string' || ad.label.length === 0){
-                        return false;
-                    }
-                    break;
-                case 'description':
-                    if (typeof ad.description !== 'string' || ad.description.length >= 200 || ad.description.length === 0) {
-                        return false;
-                    }
-                    break;
-                case 'createdAt':
-                    if (!(ad.createdAt instanceof Date)) {
-                        return false;
-                    }
-                    break;
-                case 'link':
-                    if (typeof ad.link !== 'string' || ad.link.length === 0) {
-                        return false;
-                    }
-                    break;
-                case 'vendor':
-                    if (typeof ad.vendor !== 'string' || ad.vendor.length === 0) {
-                        return false;
-                    }
-                    break;
-                case 'photoLink':
-                    if (ad.photoLink && (typeof ad.photoLink !== 'string' || ad.photoLink.length === 0)) {
-                        return false;
-                    }
-                    break;
-                case 'hashTags':
-                    if (ad.hashTags){
-                        if (!ad.hashTags.every(hashtag => typeof hashtag === 'string')){
-                            return false;
-                        }
-                    }
-                    break;
-                case 'discount':
-                    if (typeof ad.discount !== 'string' || ad.discount.length === 0) {
-                        return false;
-                    }
-                    break;
-                case 'validUntil':
-                    if (!(ad.validUntil instanceof Date)) {
-                        return false;
-                    }
-                    break;
-                case 'rating':
-                    if (!(typeof ad.rating === 'undefined' || typeof ad.rating === 'number')){
-                        return false;
-                    }
-                    break;
-                case 'reviews':
-                    if (!Array.isArray(ad.reviews)) {
-                        return false;
-                    }
-                    break;
-                default:
-                    return false;
-            }
-        }
-        return true;
-    }
-
-    add(ad){
-        if (AdsList.validate(ad)) {
-            this._adList.push(ad);
-            return true;
-        }
-        return false;
-    }
-
-    remove(id){
-        if (typeof id === 'string'){
-            let index = this._adList.findIndex(ad => ad.id === id);
-            if (index !== -1){
-                this._adList.splice(index, 1);
-    
-                return true;
-            }
-        }
-        return false;
-    }
-
-    edit(id, ad){
-        for (let param in ad){
-            if (param === 'id' || param === 'vendor' || param === 'createdAt'){
-                console.log("You can't change id, vendor, createdAt");
-                return false;
-            }
-        }
-        
-        var tmpAd = {};
-        Object.assign(tmpAd, this.get(id));
-        
-        for (let param in ad){
-            tmpAd[param] = ad[param];
-        }
-    
-        if (!AdsList.validate(tmpAd)){
-            return false;
-        }
-        
-        this.remove(id);
-        this.add(tmpAd);
-
-        return true;
-    }
-
-    addAll(adList){
-        return adList.filter(ad => !this.add(ad));
-    }
-    
-    clear(){
-        this._adList.splice(0, this._adList.length);
-    }
-}
-
 class View{
     _ads;
     _tmpAd;
@@ -200,14 +19,7 @@ class View{
             document.querySelector(".log-out").style.visibility = "visible";
         }
     }
-    _getOffer(ad){
-        let template = document.importNode(this._tmpAd, true);
-        template.querySelector(".image-in-offer").setAttribute('src', ad.photoLink);
-        template.querySelector(".percent").textContent = ad.discount + "%";
-        template.querySelector(".date-expiration").textContent = ad.validUntil.getFullYear() + " " + (ad.validUntil.getMonth() + 1) + " " + ad.validUntil.getDate();
-        template.querySelector(".offer-label").textContent = ad.label;
-        template.querySelector(".seller-name").textContent = ad.vendor;
-        template.querySelector(".seller-link").setAttribute("href", ad.link);
+    _showButtons(template){
         if(this._username == null || this._username.length == 0){
             template.querySelectorAll(".button-offer").forEach(button => button.style.visibility = "hidden");
             template.querySelector(".add-comment").style.visibility = "hidden";
@@ -218,6 +30,26 @@ class View{
         else {
             template.querySelectorAll(".button-offer").forEach(button => button.style.visibility = "hidden");
         }
+    }
+    _showReviews(template, ad){
+        let reviewsTmp = template.querySelector(".reviews-offer");
+        template.querySelectorAll(".review").forEach(rev => rev.remove());
+        ad.reviews.forEach(review => {
+            let review1 = document.createElement('span');
+            review1.className = "review";
+            review1.textContent = review;
+            reviewsTmp.append(review1);
+        });
+    }
+    _buildOffer(ad){
+        let template = document.importNode(this._tmpAd, true);
+        template.querySelector(".image-in-offer").setAttribute('src', ad.photoLink);
+        template.querySelector(".percent").textContent = ad.discount + "%";
+        template.querySelector(".date-expiration").textContent = ad.validUntil.getFullYear() + " " + (ad.validUntil.getMonth() + 1) + " " + ad.validUntil.getDate();
+        template.querySelector(".offer-label").textContent = ad.label;
+        template.querySelector(".seller-name").textContent = ad.vendor;
+        template.querySelector(".seller-link").setAttribute("href", ad.link);
+        this._showButtons(template);
         template.querySelector(".description-offer").textContent = ad.description;
         template.querySelector(".rating-number").textContent = ad.rating;
         let hashtagsTmp = template.querySelector(".hashtags-offer");
@@ -228,21 +60,15 @@ class View{
             hashtag.textContent = "#" + tag;
             hashtagsTmp.append(hashtag);
         });
-        let reviewsTmp = template.querySelector(".reviews-offer");
-        template.querySelectorAll(".review").forEach(rev => rev.remove());
-        ad.reviews.forEach(review => {
-            let review1 = document.createElement('span');
-            review1.className = "review";
-            review1.textContent = review;
-            reviewsTmp.append(review1);
-        });
+        this._showReviews(template, ad);
         return template;
     }
+
     showAds(){
         let secondButton = document.querySelector(".offers-list .button-load-more:last-child");
         document.querySelectorAll(".offer-tmp").forEach( offer => offer.remove());
         this._ads.getPage().forEach( ad => {
-            secondButton.before(this._getOffer(ad));
+            secondButton.before(this._buildOffer(ad));
         });
     }
 
@@ -335,8 +161,13 @@ let view = new View([
                             reviews : ['Эти шкафы прекрасны!'] ,
                         }
     ], "Alex popovich" , false);
+
+function showPage(){
 view.showUser();
 view.showAds();
+}
+
+function addAd(){
 view.addAd({
             id: '21',
             description : 'Скидка на столы - до 89%',
@@ -352,3 +183,6 @@ view.addAd({
             reviews : ['Эти столы прекрасны!'] ,
         }
         );
+}
+
+showPage();
